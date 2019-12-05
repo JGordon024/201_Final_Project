ce39c896c613d02fb4f9798a3b8361abf997245b
Jacob Gordon <jgordon024@west-mec.org>
2019-12-04 18:24:18 -0700

    pug file
    Worked on the pug file and integrated bootstrap. I removed bulma in favor of bootstrap. Started to make the fetch async in order to try and fix problems that were encountered in another project

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(express.static("public"))
 
-app.get('',(req,res)=>{
-    fetch('https://swapi.co/api/planets')
+app.get('', async (req,res)=>{
+    const planetNames = await fetch("https://swapi.co/api/planets") 
+    const json = planetNames.json
+
+    fetch(`https://swapi.co/api/${req.params.name}`)
     .then(res => res.json())
     .then(
         data => {
@@ -22,4 +25,15 @@ app.get('',(req,res)=>{
     )
 })
 
+// app.use('planets', async (req, res)=>{
+//     try{
+//     something = await fetch(`https://swapi.co/api/planets/${req.params.name}`)
+//     // .then(res =>res.json)
+//     // .then(data=>)
+//     const _json = await _res.json()
+//     console.log(_json)
+//     } catch(error){
+// console.log(error)
+//     }
+// })
 app.listen(port, ()=>console.log(`Server is ${port}`))
