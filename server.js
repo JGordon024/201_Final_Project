664a37058fbcd008205cc2543e762158173577c0
Jacob Gordon <jgordon024@west-mec.org>
2019-12-05 18:21:07 -0700

    Front end, some back end
    Reworked the index render so that it has variables I can call into the index.pug file. Added images and worked on the starwars.pug file.

--- server.js
+++ server.js
@@ -10,30 +10,27 @@ app.set('view engine', 'pug')
 app.use(express.static("public"))
 
 app.get('', async (req,res)=>{
-    const planetNames = await fetch("https://swapi.co/api/planets") 
-    const json = planetNames.json
+    // const planetNames = fetch("https://swapi.co/api/planets") 
+    // const json = planetNames.json
 
+
+            res.render('index',{
+                SWTitle: 'Star Wars API',
+                PokeTitle: 'Pokemon API'
+            })
+         
+})
+
+app.get('starwars', (req, res)=>{
     fetch(`https://swapi.co/api/${req.params.name}`)
     .then(res => res.json())
     .then(
         data => {
-            res.render('index',{
+            res.render('starwars',{
                 planet: data.results[1].name
             })
             console.log(data.results[1].name)
         }
     )
 })
-
-// app.use('planets', async (req, res)=>{
-//     try{
-//     something = await fetch(`https://swapi.co/api/planets/${req.params.name}`)
-//     // .then(res =>res.json)
-//     // .then(data=>)
-//     const _json = await _res.json()
-//     console.log(_json)
-//     } catch(error){
-// console.log(error)
-//     }
-// })
 app.listen(port, ()=>console.log(`Server is ${port}`))
